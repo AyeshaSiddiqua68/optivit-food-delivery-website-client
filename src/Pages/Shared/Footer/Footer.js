56a6014b48618e482321f13dd815ac479101d385
AyeshaSiddiqua68 <akhiazad6868@gmail.com>
2021-10-29 02:44:59 +0600

    first commit

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <h3>footer for all pages</h3>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+           <h3>header for all pages</h3> 
+        </div>
+    );
+};
+
+export default Header;
