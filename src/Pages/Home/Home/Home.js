a668c64dc0b827a2d19f2df5da1a57753a3817d7
AyeshaSiddiqua68 <akhiazad6868@gmail.com>
2021-10-31 16:42:42 +0600

    home page done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Services from '../Services/Services';
-
+import Banner from '../Banner/Banner';
+import ThirdSection from '../ThirdSection/ThirdSection';
+import FifthSection from '../FifthSection/FifthSection';
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
+            <ThirdSection></ThirdSection>
             <Services></Services>
+            <FifthSection></FifthSection>
         </div>
     );
 };
---
+++ src/Pages/Home/ThirdSection/ThirdSection.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './ThirdSection.css'
+import image from '../../../images/thirdSection/Image-1.jpg'
+import logo1 from '../../../images/thirdSection/logo1.png'
+import logo2 from '../../../images/thirdSection/logo2.png'
+
+const ThirdSection = () => {
+    return (
+        <div className="container">
+           
+            <div className="row">
+                <div  className="col-lg-6 col-sm-6 col-6 d-flex justify-content-center align-items-center  ">
+                    <div>
+                        <h1 className='section-title'>Eat whatever, eat whenever.</h1>
+                        <h6>Our company is engaged in the delivery of healthy and tasty food around the city. Special cooking and delivery technologies allow you to buy fresh and healthy food. Experienced chefs and professional couriers will provide you with a nutritious meal.</h6>
+                        <br />
+                        <div className="row">
+                        <h1 className="section-p">Available at Mobile App</h1>
+                            <div className="col-md-6">
+                                <img src={logo1} alt="" />
+                            </div>
+                            <div className="col-md-6">
+                            <img src={logo2} alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <img src={image} alt="" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ThirdSection;
