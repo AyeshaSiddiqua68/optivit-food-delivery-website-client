721b451456e8852df58b0d54d19eaaa9312c8726
AyeshaSiddiqua68 <akhiazad6868@gmail.com>
2021-11-18 02:23:19 +0600

    responsive done

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -4,96 +4,94 @@ import { Carousel } from 'react-bootstrap';
 import banner1 from '../../../images/banner/banner8.jpg'
 import banner2 from '../../../images/banner/banner7.jpg'
 import banner3 from '../../../images/banner/banner9.jpg'
+import Bounce from "react-reveal/Bounce";
 
 //home page banner part
 const Banner = () => {
     return (
-        <div className="w-100 mx-auto">
+        <>
             <Carousel variant="dark">
-                <Carousel.Item className="banner-image col-lg-12 col-sm-12 col-12">
+                <Carousel.Item>
                     <img
-                        className="d-block w-100 "
-                        src={banner1}
+                        className="d-block w-100"
+                        style={{ height: "600px" }}
+                        src={banner2}
                         alt="First slide"
                     />
+
                     <Carousel.Caption>
-                        <div className="row">
-                            <div className="col-lg-6"></div>
-
-                            <div className="col-lg-6 col-sm-6 col-6 d-flex justify-content-center align-items-center  ">
-                                <div className="banner-section p-4 w-100 mx-auto my-auto ">
-                                    <h1 className='carousel-title'>You Hungry?</h1>
-
-                                    <h1 className='carousel-p p-4'>Let's find something healthy delicious for you.</h1>
-                                    {/* <p ><small>We love making our customers happy by serving them our tasty food! High-quality ingredients mixed with excellent service is the best recipe for a successful food truck.</small></p> */}
-                                    <div>
+                        <div className="row mx-auto carousel-caption">
+                            <div className="align-items-center justify-content-center" >
+                                <Bounce left cascade>
+                                    <h1 className='carousel-title text-center  fw-bold'>Are You Hungry?</h1></Bounce>
+                                <Bounce right cascade>
+                                    <p className='carousel-p p-4 text-center text-white fw-bold'>Let's find something healthy delicious for you.</p></Bounce>
+                                <Bounce>
                                     <p>
-                                    <input type="text" className="loaction-btn" placeholder="Enter Your Location" />
-                                    <button className="search-btn">Search</button>
+                                        <input type="text" className="loaction-btn" placeholder="Enter Your Location" />
+                                        <button className="search-btn">Search</button>
                                     </p>
-                                    </div>
-                                </div>
+                                </Bounce>
 
                             </div>
-
                         </div>
                     </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item className="banner-image col-lg-12 col-sm-12 col-12">
+                <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src={banner2}
+                        style={{ height: "600px" }}
+                        src={banner1}
                         alt="Second slide"
                     />
-
                     <Carousel.Caption>
-                        <div className="row">
-                            
-
-                            <div className="col-lg-6 col-sm-6 col-6 d-flex justify-content-center align-items-center  ">
-                                <div className="banner-section p-4 ">
-                                    <h1 className='carousel-title'>Want Healthy?</h1>
-
-                                    <h1 className='carousel-p p-4'>Order healthy and fresh food any time.</h1>
-                                    <p ><small>We love making our customers happy by serving them our tasty food! High-quality ingredients mixed with excellent service is the best recipe for a successful food truck.</small></p>
-                                    <input type="text" className="loaction-btn" placeholder="Enter Your Location" />
-                                    <button className="search-btn">Search</button>
-                                </div>
+                        <div className="row mx-auto carousel-caption">
+                            <div className="align-items-center justify-content-center" >
+                                <Bounce left cascade>
+                                    <h1 className='carousel-title text-center fw-bold'>Want Something Healthy?</h1></Bounce>
+                                <Bounce right cascade>
+                                    <p className='carousel-p p-4 text-center text-white fw-bold'>Order healthy and fresh food any time.</p></Bounce>
+                                <Bounce>
+                                <p>
+                                        <input type="text" className="loaction-btn" placeholder="Enter Your Location" />
+                                        <button className="search-btn">Search</button>
+                                    </p>
+                                </Bounce>
 
                             </div>
-                            <div className="col-lg-6"></div>
-
                         </div>
                     </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item className="banner-image col-lg-12 col-sm-12 col-12">
+
+                <Carousel.Item>
                     <img
                         className="d-block w-100"
+                        style={{ height: "600px" }}
                         src={banner3}
                         alt="Third slide"
                     />
                     <Carousel.Caption>
-                        <div className="row">
-                            <div className="col-lg-6"></div>
-
-                            <div className="col-lg-6 col-sm-6 col-6 d-flex justify-content-center align-items-center  ">
-                                <div className="banner-section p-4 ">
-                                    <h1 className='carousel-title'>Want Different?</h1>
-
-                                    <h1 className='carousel-p p-4'>Eat whatever, eat whenever as your wish!</h1>
-                                    <p ><small>We love making our customers happy by serving them our tasty food! High-quality ingredients mixed with excellent service is the best recipe for a successful food truck.</small></p>
-                                    <input type="text" className="loaction-btn" placeholder="Enter Your Location" />
-                                    <button className="search-btn">Search</button>
-                                </div>
+                        <div className="row mx-auto carousel-caption">
+                            <div className="align-items-center justify-content-center" >
+                                <Bounce left cascade>
+                                    <h1 className='carousel-title text-center fw-bold'>Looking For Different?</h1></Bounce>
+                                <Bounce right cascade>
+                                    <p className='carousel-p p-4 text-center text-white fw-bold'>Eat whatever, eat whenever as your wish!</p></Bounce>
+                                <Bounce>
+                                <p>
+                                        <input type="text" className="loaction-btn" placeholder="Enter Your Location" />
+                                        <button className="search-btn">Search</button>
+                                    </p>
+                                </Bounce>
 
                             </div>
-
                         </div>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
-        </div>
+        </>
     );
 };
 
 export default Banner;
+
